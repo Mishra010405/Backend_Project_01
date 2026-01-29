a10e3c1baec13f5c29119e54dd2716fc448bf28e
Shivam Mishra <mshivam2062@gmail.com>
2026-01-29 23:40:56 +0530

    SOme of the Basic String Questions

--- public/src/controllers/user.controller.js
+++ public/src/controllers/user.controller.js
@@ -2,8 +2,20 @@ import {asynchandler} from "../utils/asynchandler.js";
 
 const registerUser = asynchandler(async (req,res) => {
     res.status(200).json({
-        message:"Ok"
+        message:"Ok,My name is Shivam Mishra"
+        // get the user Detailed from fruntend.
+        // vlidation- not empty
+        // Check if the user already exist.
+        // Check for images , check for avtar.
+        // Upload them to cloudinary, avtar
+        // creae user object - create enyrt in db
+        // remove password and refresh token field from the response
+        // Check for user creation
+        // return res
     })
+
+    const {fullname  , username, email, password} = req.body
+    console.log(email);
 })
 
 
