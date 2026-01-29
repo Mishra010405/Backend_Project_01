e62b2923819b29e4a038be8671d240e78ccd4fa6
Shivam Mishra <mshivam2062@gmail.com>
2026-01-30 00:07:14 +0530

    SOme of the Basic String Questions

--- public/src/controllers/user.controller.js
+++ public/src/controllers/user.controller.js
@@ -1,4 +1,7 @@
+import { application } from "express";
 import {asynchandler} from "../utils/asynchandler.js";
+import {ApiError} from "../utils/Apierrors.js";
+import {User} from "../models/User.models.js";
 
 const registerUser = asynchandler(async (req,res) => {
     res.status(200).json({
@@ -16,6 +19,27 @@ const registerUser = asynchandler(async (req,res) => {
 
     const {fullname  , username, email, password} = req.body
     console.log(email);
+
+    if(!fullname || fullname.trim() ===" "){
+        throw new ApiError(400, "Full Name is required")
+    }
+    if(!username || username.trim() ===" "){
+        throw new ApiError(400, "UserName is required")
+    }
+    if(!email || email.trim() ===" "){
+        throw new ApiError(400, "Email is required")
+    }
+    if(!password || password.trim() ===" "){
+        throw new ApiError(400, "Password is required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{username} , {email}]
+    })
+
+    if(existedUser){
+        throw new ApiError(400,"User with email or all user existed")
+    }
 })
 
 
