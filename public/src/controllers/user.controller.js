cd3fc05ac84eb6ce96a43b6fb83d3d455b7261f6
Shivam Mishra <mshivam2062@gmail.com>
2026-01-30 11:10:44 +0530

    SOme of the Basic String Questions

--- public/src/controllers/user.controller.js
+++ public/src/controllers/user.controller.js
@@ -2,6 +2,8 @@ import { application } from "express";
 import {asynchandler} from "../utils/asynchandler.js";
 import {ApiError} from "../utils/Apierrors.js";
 import {User} from "../models/User.models.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asynchandler(async (req,res) => {
     res.status(200).json({
@@ -40,6 +42,42 @@ const registerUser = asynchandler(async (req,res) => {
     if(existedUser){
         throw new ApiError(400,"User with email or all user existed")
     }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath) {
+        throw new ApiError(400,"Avatar file is required")
+    }
+    
+    const avatar = await uploadonCLoudinary(avatarLocalPath)
+    const coverImage = await uploadonCLoudinary
+    (coverImageLocalPath)
+    
+    if(!avatar) {
+        throw new ApiError(400,"Avatar file is required")
+    }
+
+    const user = await User.create({
+        fullname,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+
+    const createUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createUser) {
+        throw new ApiError(500, "Something went wrong while registering the user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createUser, "User registered Successfully ")
+    )
 })
 
 
